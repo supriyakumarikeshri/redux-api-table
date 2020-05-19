bf40cab068978b89d54831890ba9e9a8e72754bc
supriyakumarikeshri <supriyakumari2393@gmail.com>
2020-05-19 18:06:36 +0530

    Updates

---
+++ precache-manifest.fc421b4163c8b3589380038e16aef0f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f638318023d43ce7ed740e20bf9de94",
+    "url": "/redux-api-table/index.html"
+  },
+  {
+    "revision": "ff8f7bba0faeccb17a33",
+    "url": "/redux-api-table/static/css/main.bd84e5cf.chunk.css"
+  },
+  {
+    "revision": "1b6fea7c9d1e18a66b7e",
+    "url": "/redux-api-table/static/js/2.93da5c12.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-api-table/static/js/2.93da5c12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff8f7bba0faeccb17a33",
+    "url": "/redux-api-table/static/js/main.93b22789.chunk.js"
+  },
+  {
+    "revision": "7c69b60ac90053b82574",
+    "url": "/redux-api-table/static/js/runtime-main.0ebc8b50.js"
+  }
+]);
